8260050ffffcb1ac8e601f6fe4e6ae632da2d544
Vignesh_Ss <56245530+vigneshSs-07@users.noreply.github.com>
2021-08-18 21:13:46 +0530

    basic class and objects

---
+++ ObjectsAndClasses/demo.py
@@ -0,0 +1,11 @@
+
+def add_num(a,b):
+    return a + b
+
+a = 10
+b= 99
+
+if __name__ == "__main__" :    
+    print("welcome user")
+    print("The Sum of numbers is: {}".format(add_num(a,b)))
+    print(__name__)
